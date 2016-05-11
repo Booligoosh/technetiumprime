910812bbb8e6d94d8f15aa527adba5ea3e6228ec
Booligoosh <booligoosh@optusnet.com.au>
2016-05-11 16:36:22 +1000

    started cookies foundation

--- primegeneratornew.js
+++ primegeneratornew.js
@@ -1,10 +1,21 @@
-var num = 3;
-var df = 1;
-var done = false;
-var prime = false;
-var primecount = 0;
-var testNumSqurt = 0;
-var primes = ["filler"];
+if (document.cookie === "") {
+    var num = 3;
+    var df = 1;
+    var done = false;
+    var prime = false;
+    var primecount = 0;
+    var testNumSqurt = 0;
+    var primes = ["filler"];
+}
+else {
+    var num = getCookie("num");
+    var df = 1;
+    var done = false;
+    var prime = false;
+    var primecount = getCookie("primecount");
+    var testNumSqurt = 0;
+    var primes = getCookie("primes");
+}
 addtolist(2);
 addtolist(3);
 start();
@@ -16,6 +27,7 @@ function start() {
 
 function nextprime() {
     num = num + 2;
+    setCookie("num",num,365);
     checkprime();
 }
 
@@ -43,6 +55,8 @@ function checkprime() {
     }
 }
 
+//the following functions have been taken or modified from http://www.w3schools.com/
+
 function addtolist(data) {
     primecount = primecount + 1;
     var li = document.createElement("LI");
@@ -51,4 +65,28 @@ function addtolist(data) {
     document.getElementById("primeslist").appendChild(li);
     document.getElementById("primecount").innerHTML = primecount + " primes calculated so far";
     primes.push(data);
+    setCookie("primes",primes[],365);
+    setCookie("primecount",primecount,365);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
