b0daea6ea3f0c4a88b47058508e5bc1554ec7a67
Booligoosh <booligoosh@optusnet.com.au>
2016-05-18 11:41:00 +1000

    removed w3schools cookie functions

--- primegeneratorcookies.js
+++ primegeneratorcookies.js
@@ -91,25 +91,3 @@ function rawListAdd (data) {
     document.getElementById("primeslist").appendChild(li);
     document.getElementById("primecount").innerHTML = primecount + " primes calculated so far";
 }
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = document.cookie + cname + "=" + cvalue + "; " + expires;
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    //return "";
-}
